24080f9c4edb879b84da8c3bed5133988708a853
artur.reinke <artur.reinke@logon.pl>
2020-11-03 22:25:03 +0100

    Add project files.

---
+++ Commander/Controllers/CommandsController.cs
@@ -0,0 +1,42 @@
+﻿using Commander.Data;
+using Commander.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Commander.Controllers
+{   
+    [Route("api/commands")]
+    [ApiController]
+    public class CommandsController : ControllerBase
+    {
+        private readonly ICommanderRepo _repository;
+
+        public CommandsController(ICommanderRepo repository)
+        {
+            _repository = repository;
+        }
+
+        private readonly MockCommanderRepo mockCommanderRepo = new MockCommanderRepo();
+
+        //GET api/commands
+        [HttpGet]
+        public ActionResult <IEnumerable<Command>> GetCommands() // czemu 2x << >>
+        {
+            var commandItems = _repository.GetCommands();
+
+            return Ok(commandItems);
+        }
+        //GET api/commands/5
+        [HttpGet("{id}")]
+        public ActionResult <Command> GetCommandById(int id)
+        {
+            var commandItem = _repository.GetCommandById(id);
+
+            return Ok(commandItem);
+            
+        }
+    }
+}
---
+++ Commander/Data/MockCommanderRepo.cs
@@ -0,0 +1,29 @@
+﻿using Commander.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Commander.Data
+{
+    public class MockCommanderRepo : ICommanderRepo
+    {
+        public Command GetCommandById(int id)
+        {
+            //Command com = new Command();
+            return new Command { Id = 0, HowTo = "Boil An Egg", Line = "Boil Water", Platform = "Kettle & Pan" };
+        }
+
+        public IEnumerable<Command> GetCommands()
+        {
+            var commands = new List<Command>()
+            {
+            new Command { Id = 0, HowTo = "Boil An Egg", Line = "Boil Water", Platform = "Kettle & Pan" },
+            new Command { Id = 1, HowTo = "Cut Bread", Line = "Get a knife", Platform = "Knife & chopping board" },
+            new Command { Id = 2, HowTo = "Make cup of tea", Line = "place teabag in cup", Platform = "Kettle & cup" }
+            };
+
+            return  commands;
+        }
+    }
+}
